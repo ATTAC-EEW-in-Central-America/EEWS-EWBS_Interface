cedf206bda6f1d2b12052bf95cf3fdbdb8f0445e
F Massin <fmassin@sed.ethz.ch>
2022-09-22 05:00:08 +0200

    up

--- dashboard/dashboard_deltatimes.py
+++ dashboard/dashboard_deltatimes.py
@@ -86,12 +86,8 @@ def setScreenSize(sWidth=sWidth,sHeight=sHeight):
 		log('this is Linux OS')
 		import subprocess
 		import re
-		matchObj=False
-		try:
-			xrandrOutput = str(subprocess.Popen(['xrandr'], stdout=subprocess.PIPE).communicate()[0])
-			matchObj = re.findall(r'current\s(\d+) x (\d+)', xrandrOutput)
-		except:
-			print('Cannot use xrandr')
+		xrandrOutput = str(subprocess.Popen(['xrandr'], stdout=subprocess.PIPE).communicate()[0])
+		matchObj = re.findall(r'current\s(\d+) x (\d+)', xrandrOutput)
 		if matchObj:
 			sWidth = int(matchObj[0][0])
 			sHeight = int(matchObj[0][1])
